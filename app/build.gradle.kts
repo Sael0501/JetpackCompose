163a8c9152696eabc108d7ba998f0bbbbc4f1598
Sael Aceves <95884579+Sael0501@users.noreply.github.com>
2025-07-06 18:56:58 -0600

    Image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.constraint.layout)
+    implementation(libs.coil)
+    implementation(libs.coil.network)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
